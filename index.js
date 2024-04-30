768c6e35a987588931c5d3b9849b43f9ebb157cc
slava <tfdf63@yandex.ru>
2024-04-30 11:20:29 +0400

    update index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // const tick = require('./4tick.js')
+const fetch = require('node-fetch'); // Импортируем модуль fetch
 require('dotenv').config();
 //Логика билетов
 const crypto = require('crypto');
@@ -16,41 +17,27 @@ const hashedPassword = crypto
 const resultString = `${login}:${hashedPassword}:${timestamp}`;
 //Название команды
 let game = 'Акрон х Динамо // 12.05.2024';
-//Сумма билетов
-let ticketsSum = 0;
-let ticketsAllCount = 0;
-let ticketsCount = 0;
-let ticketsFreeCount = 0;
 
-const url = `https://api.tickets.yandex.net/api/crm/?action=crm.order.ticket.list&auth=${resultString}&city_id=3296193&event_id=17771827`;
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    if ((data.status = '0')) {
-      const results = data.result;
-      //Количество билетов
-      ticketsAllCount = results.length;
-      let filterData = results.filter((item) => item.price !== 0);
-      ticketsCount = filterData.length;
-      ticketsFreeCount = ticketsAllCount - ticketsCount;
+async function ticketsGetInfo(resultString) {
+  const url = `https://api.tickets.yandex.net/api/crm/?action=crm.order.ticket.list&auth=${resultString}&city_id=3296193&event_id=17771827`;
+  const response = await fetch(url);
 
-      results.forEach((result) => {
-        ticketsSum += result.price;
-      });
-      game = 'Акрон х Динамо / 12.05.2024';
-      ticketsSum = ticketsSum;
-      ticketsCount = ticketsCount;
-      ticketsFreeCount = ticketsFreeCount;
-      ticketsAllCount = ticketsAllCount;
-    } else {
-      console.error('Ошибка в ответе API:', data.error);
-    }
-  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  if (data.status === '0') {
+    const results = data.result;
+    const tickets = {
+      allCount: results.length,
+      count: results.filter((item) => item.price !== 0).length,
+    };
+    return tickets;
+  } else {
+    throw new Error(`Ошибка в ответе API: ${data.error}`);
+  }
+}
 //Завершение логики билетов
 
 const { Bot, GrammyError, HttpError } = require('grammy');
@@ -74,26 +61,39 @@ bot.command('start', async (ctx) => {
     'Привет! Я - бот ФК "Акрон". Молодой и звонкий! <span class="tg-spoiler">Хорошего дня!</span>',
     {
       parse_mode: 'HTML',
-      disable_web_page_priview: true,
+      disable_web_page_preview: true,
     }
   );
 });
 
 bot.command('tickets', async (ctx) => {
-  await ctx.reply(
-    `
-  <b>${game}</b>
+  try {
+    // Получаем данные о билетах, передавая resultString из глобальной области видимости
+    const tickets = await ticketsGetInfo(resultString);
 
-  Продано: <b>${ticketsCount}</b>
-  Пригласительных: <b>${ticketsFreeCount}</b>
+    await ctx.reply(
+      `
+      <b>${game}</b>
 
-  Итого: <b>${ticketsAllCount}</b>
-  `,
-    {
-      parse_mode: 'HTML',
-      disable_web_page_priview: true,
-    }
-  );
+      Продано: <b>${tickets.count}</b>
+      Пригласительных: <b>${tickets.allCount - tickets.count}</b>
+
+      Итого: <b>${tickets.allCount}</b>
+      `,
+      {
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+      }
+    );
+  } catch (error) {
+    console.error(
+      'Произошла ошибка при получении информации о билетах:',
+      error
+    );
+    await ctx.reply(
+      'Извините, произошла ошибка при получении информации о билетах.'
+    );
+  }
 });
 // <b>${ticketsSum}руб.</b>
 bot.hears(/пипец/, async (ctx) => {
